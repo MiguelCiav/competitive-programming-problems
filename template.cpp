783a8599b1d2091e6becc0bb4be0e2f92c696ea7
Miguel Ciavato <miguelciavato@gmail.com>
2025-11-13 12:29:04 -0400

    fixed template

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = std::vector<int>;
+using vll = std::vector<ll>;
+using vull = std::vector<ull>;
+using mati = std::vector<std::vector<int>>;
+using matll = std::vector<std::vector<ll>>;
+using matull = std::vector<std::vector<ull>>;
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    
+    return 0;
+}
