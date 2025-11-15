4751c27e04cb785d3b9008ce7b8d9d544890132c
Miguel Ciavato <miguelciavato@gmail.com>
2025-11-15 15:37:10 -0400

    some advances in UVA-10911

--- CODEFORCES_SOLVED/800_DIFFICULTY/546A/link_and_explaining.md
+++ CODEFORCES_SOLVED/800_DIFFICULTY/546A/link_and_explaining.md
@@ -21,3 +21,15 @@ $$
 Para saber cuanto hay que pedir prestado, basta con restar el costo total $C$ a los dolares que se tienen $n$. Si ocurre que $C-n \leq 0$, entonces no hay que pedir prestado.
 
 English:
+
+- w -> number of bananas to buy
+- k -> price multiplier: banana 1 costs 1·k, banana 2 costs 2·k, ..., banana i costs i·k
+- n -> number of dollars you have
+
+The total cost of the bananas C is the sum ∑_{i=1}^{w} i·k, i.e.
+
+$$
+C = \sum_{i=1}^{w} i k = k\left(\sum_{i=1}^{w} i\right) = k\left(\frac{w(w+1)}{2}\right)
+$$
+
+The amount to borrow is C − n, but if C − n ≤ 0 then nothing needs to be borrowed. In other words, the amount to borrow is max(0, C − n).
---
+++ UVA-10911.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using ll = long long;
+using ii = std::pair<int,int>;
+using vi = std::vector<int>;
+using vll = std::vector<ll>;
+using vii = std::vector<ii>;
+using mati = std::vector<std::vector<int>>;
+using matll = std::vector<std::vector<ll>>;
+
+const float INF = 1e9;
+
+float distance(ii a, ii b){
+    return std::sqrt(std::pow(a.first - b.first, 2) + std::pow(a.second - b.second, 2));
+}
+
+float dp(short combination, vii &students, int index, vi &memo) {
+    if(combination == 0) return 0;
+    if(memo[combination] != -1) return memo[combination];
+    float min = INF;
+    
+}
+
+// 0 -> primera pos
+// 
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+    int number_of_students, _case = 1;
+    std::cin >> number_of_students;
+    short combination = std::pow(2, number_of_students) - 1;
+    while(number_of_students != 0){
+        vii students;
+        vi memo = vi(256, -1);
+        std::string ignore;
+        int point_x, point_y;
+
+        for(int i = 0; i < number_of_students; i++) {
+            std::cin >> ignore >> point_x >> point_y;
+            students.push_back({point_x, point_y});
+        }
+        std::cout << "Case " << _case << ": " << dp(combination,students,0,memo) << "\n";
+
+        _case++;
+        std::cin >> number_of_students;
+        combination = std::pow(2, number_of_students) - 1;
+    }
+    return 0;
+}
