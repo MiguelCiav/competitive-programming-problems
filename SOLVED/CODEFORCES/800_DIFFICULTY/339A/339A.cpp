b73242e12e552a83ef21d363aa7633078cf57cf9
Miguel Ciavato <miguelciavato@gmail.com>
2025-11-25 18:21:03 -0400

    just finished problem 339A

--- SOLVED/CODEFORCES/800_DIFFICULTY/339A/339A.cpp
+++ SOLVED/CODEFORCES/800_DIFFICULTY/339A/339A.cpp
@@ -14,6 +14,18 @@ using matll = vector<vector<ll>>;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+    vi v;
+    string s;
+    cin >> s;
+    for(auto c : s) {
+        if(c == '+') continue;
+        v.push_back(c - '0');
+    }
+    sort(v.begin(), v.end());
+    for(size_t i = 0; i < v.size(); i++) {
+        cout << v[i];
+        if(i != v.size() - 1) cout << "+";
+    }
+    cout << endl;
     return 0;
 }
