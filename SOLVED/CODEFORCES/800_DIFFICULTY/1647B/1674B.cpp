ccbe3901098bfb5372be307d4271ab61692dc518
Miguel Ciavato <miguelciavato@gmail.com>
2025-11-26 13:59:30 -0400

    just finished problem 1674B

--- SOLVED/CODEFORCES/800_DIFFICULTY/1647B/1674B.cpp
+++ SOLVED/CODEFORCES/800_DIFFICULTY/1647B/1674B.cpp
@@ -14,6 +14,12 @@ using matll = vector<vector<ll>>;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+    short t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        cout << (25*(s[0]-97) + 1) + (s[1]-97) - (s[1] > s[0] ? 1 : 0) << '\n'; 
+    }
     return 0;
 }
