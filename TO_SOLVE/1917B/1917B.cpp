f5c427dc7e8c01e3a0f5344b0db645e4dd8cf208
Miguel Ciavato <miguelciavato@gmail.com>
2025-12-09 23:05:04 -0400

    just started problem 1917B

---
+++ TO_SOLVE/1917B/1917B.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ii = pair<int,int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vii = vector<ii>;
+using mati = vector<vector<int>>;
+using matll = vector<vector<ll>>;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
